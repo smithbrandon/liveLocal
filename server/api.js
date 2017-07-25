f38dd4b3c2be173af5dd0ca86192f580f8302801
judsonhockman <judson.hockman@gmail.com>
2017-07-25 16:23:03 -0400

    commit w/api.js included

--- server/api.js
+++ server/api.js
@@ -1,5 +1,14 @@
 var express= require('express');
+var categories = require('./controllers/categories.ctrl');
+var events = require('./controllers/events.ctrl');
+var tags = require('./controllers/tags.ctrl');
+var users = require('./controllers/users.ctrl');
+
 
 var router = express.Router();
+router.use('/categories', categories);
+router.use('/events', events);
+router.use('/tags', tags);
+router.use('/users', users);
 
 module.exports = router;
