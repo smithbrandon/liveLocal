5555550e0b2c0053e50b31ca426e69d2d517c789
jlovedean <jlovedean@gmail.com>
2017-07-24 23:44:26 -0400

    initial setup commit

---
+++ client/app.js
@@ -0,0 +1,24 @@
+angular.module('events', ['ngRoute', 'ngResource', 'events.controllers', 'events.factories', 'events.services'])
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+    .when('/', {
+        templateUrl: 'views/welcome.html',
+        controller: 'WelcomeController'
+    })
+    // .when('', {
+    //     templateUrl: 'views/list.html',
+    //     controller: 'EventListController'
+    // })
+    // .when('/compose', {
+    //     templateUrl: 'views/compose.html',
+    //     controller: 'ComposeEventController'
+    // })
+    // .when('/:id', {
+    //     templateUrl: 'views/single.html',
+    //     controller: 'SingleEventController'
+    // })
+    .otherwise({
+        redirectTo: '/'
+    });
+}]);
