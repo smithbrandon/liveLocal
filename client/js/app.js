61af5316457b8d7dc2e6804b9f3719aa29a2efc1
Brandon Smith <smith.brandon.e.82@gmail.com>
2017-07-28 11:41:40 -0400

    merge conflict resolve

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:client/app.js
-angular.module('events', ['ngRoute', 'ngResource', 'events.controllers', 'events.factories', 'events.services', 'angular-flexslider'])
-=======
 angular.module('events', ['ngRoute', 'ngResource', 'events.controllers', 'events.factories', 'events.services','angular-flexslider'])
->>>>>>> 9d357be9716e500934844ad5057a01b8be4b51f8:client/js/app.js
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
